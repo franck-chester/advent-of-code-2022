47f4dbef8c43aebf414a2e1360cf032dadfccce4
franckchester <81856476+franck-chester@users.noreply.github.com>
2022-12-05 07:17:07 +0000

    processed crates - now moves

--- day05/Day05.ts
+++ day05/Day05.ts
@@ -2,7 +2,7 @@ import { Day } from "../lib/Day";
 
 export class Day05 extends Day {
     part1Example(): string {
-        return "???"
+        return "CMZ"
     }
     part2Example(): string {
         return "???"
@@ -10,8 +10,35 @@ export class Day05 extends Day {
 
     part1(entries : string[]): string {
         let solution = "????"
+        let mode : "crateProcessing"|"moveProcessing" = "crateProcessing";
+        let stacks : string[][] = [[]];
         for (let i = 0; i < entries.length; i++) {
-            console.log(`entry = ${entries[i]}`);
+            let entry = entries[i];
+
+            if(entry.startsWith(" 1")){
+                stacks.splice(0,1) ; // have an empty row in there
+                for(let s=0; s< stacks.length; s++){
+                    console.log(` ${stacks[s].join(' ')}`);
+                }
+                console.log(entries[i].split('   ').join(' '));
+                i+=2;
+                entry = entries[i];
+                mode = "moveProcessing" ;
+                console.log(`MODE : ${mode}`);
+
+                
+            }
+
+            const regex = /(\[\w\]|\s{3})/gm;
+            if(mode == "crateProcessing"){
+             
+                let matches = entry.match(regex);
+                console.log(`REGEX MATCH : ${matches}`);
+                let crates = matches!.map(m => m.charAt(1));
+                
+                stacks.push(crates);
+            }
+            
         }
         return `${solution}`;
     };
